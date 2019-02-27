cc794cdf30dbf59c349afb3e5e815aab3eb95c57
My Kim Bui <mykimbuibui@gmail.com>
2019-02-27 10:21:34 +0100

    images on click

--- app/assets/javascripts/ball.js
+++ app/assets/javascripts/ball.js
@@ -1,104 +1,104 @@
-function detectSpecs() {
-  prompt = document.querySelector('#prompt')
-  var hasWebgl = (function() {
-    try {
-      return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl')
-    } catch (e) {
-      return false
-    }
-  })()
+// function detectSpecs() {
+//   prompt = document.querySelector('#prompt')
+//   var hasWebgl = (function() {
+//     try {
+//       return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl')
+//     } catch (e) {
+//       return false
+//     }
+//   })()
 
-  var hasGetUserMedia = (function() {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)
-  })()
+//   var hasGetUserMedia = (function() {
+//     return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)
+//   })()
 
-  if (!hasGetUserMedia) {
-    prompt.innerHTML = 'This demo requires webcam support (Chrome or Opera).'
-  } else if (!hasWebgl) {
-    prompt.innerHTML = 'No WebGL support detected. Please try restarting the browser.'
-  } else {
-    prompt.innerHTML = 'Please allow camera access.'
-    init()
-  }
-}
+//   if (!hasGetUserMedia) {
+//     prompt.innerHTML = 'This demo requires webcam support (Chrome or Opera).'
+//   } else if (!hasWebgl) {
+//     prompt.innerHTML = 'No WebGL support detected. Please try restarting the browser.'
+//   } else {
+//     prompt.innerHTML = 'Please allow camera access.'
+//     init()
+//   }
+// }
 
-const vidWidth = 256
-const vidHeight = 256
+// const vidWidth = 256
+// const vidHeight = 256
 
-const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  alpha: true
-})
+// const renderer = new THREE.WebGLRenderer({
+//   antialias: true,
+//   alpha: true
+// })
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setClearColor(0x000000, 0)
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// renderer.setPixelRatio(window.devicePixelRatio)
+// renderer.setClearColor(0x000000, 0)
 
-const sectionTag = document.querySelector("section")
-sectionTag.appendChild(renderer.domElement)
+// const sectionTag = document.querySelector("section")
+// sectionTag.appendChild(renderer.domElement)
 
-const video = document.createElement('video')
-video.width = vidWidth
-video.height = vidHeight
-video.autoplay = true
-video.loop = true
+// const video = document.createElement('video')
+// video.width = vidWidth
+// video.height = vidHeight
+// video.autoplay = true
+// video.loop = true
 
-window.URL = window.URL || window.webkitURL
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
-navigator.getUserMedia({
-  video: true
-}, function(stream) {
-  video.srcObject = stream
-  prompt.style.display = 'none'
-}, function(error) {
-  prompt.innerHTML = 'Unable to capture WebCam. Please reload the page.'
-})
+// window.URL = window.URL || window.webkitURL
+// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+// navigator.getUserMedia({
+//   video: true
+// }, function(stream) {
+//   video.srcObject = stream
+//   prompt.style.display = 'none'
+// }, function(error) {
+//   prompt.innerHTML = 'Unable to capture WebCam. Please reload the page.'
+// })
 
-videoTexture = new THREE.Texture(video)
-videoTexture.minFilter = THREE.LinearFilter
-videoTexture.magFilter = THREE.LinearFilter
+// videoTexture = new THREE.Texture(video)
+// videoTexture.minFilter = THREE.LinearFilter
+// videoTexture.magFilter = THREE.LinearFilter
 
 
-const scene = new THREE.Scene()
+// const scene = new THREE.Scene()
 
-const ambientLight = new THREE.AmbientLight(0x777777)
-scene.add(ambientLight)
+// const ambientLight = new THREE.AmbientLight(0x777777)
+// scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xFFFFFF, 1, 0)
-pointLight.position.set(500,500,-2000)
-scene.add(pointLight)
+// const pointLight = new THREE.PointLight(0xFFFFFF, 1, 0)
+// pointLight.position.set(500,500,-2000)
+// scene.add(pointLight)
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000)
-camera.position.z = -3000
+// const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000)
+// camera.position.z = -3000
 
-const ball = function() {
-  const geometry = new THREE.SphereGeometry(600, 128, 120)
-  const material = new THREE.MeshLambertMaterial({
-    map : videoTexture
-  })
+// const ball = function() {
+//   const geometry = new THREE.SphereGeometry(600, 128, 120)
+//   const material = new THREE.MeshLambertMaterial({
+//     map : videoTexture
+//   })
 
-  const mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
-  return mesh
-}
+//   const mesh = new THREE.Mesh(geometry, material)
+//   scene.add(mesh)
+//   return mesh
+// }
 
-const sphere = ball()
+// const sphere = ball()
 
-const animate = function() {
-  camera.lookAt(scene.position)
-  if (video.readyState === video.HAVE_ENOUGH_DATA) {
-    videoTexture.needsUpdate = true
-  }  requestAnimationFrame(animate)
-  // sphere.position.x += 0.001
-  // sphere.position.y += 1
-  // sphere.position.z += 1
-  renderer.render(scene, camera)
-}
+// const animate = function() {
+//   camera.lookAt(scene.position)
+//   if (video.readyState === video.HAVE_ENOUGH_DATA) {
+//     videoTexture.needsUpdate = true
+//   }  requestAnimationFrame(animate)
+//   // sphere.position.x += 0.001
+//   // sphere.position.y += 1
+//   // sphere.position.z += 1
+//   renderer.render(scene, camera)
+// }
 
-animate()
+// animate()
 
-window.addEventListener("resize", function() {
-  camera.aspect = window.innerWidth/innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+// window.addEventListener("resize", function() {
+//   camera.aspect = window.innerWidth/innerHeight
+//   camera.updateProjectionMatrix()
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+// })
--- app/assets/javascripts/imgs.js
+++ app/assets/javascripts/imgs.js
@@ -1,48 +1,54 @@
 const imagesFist = document.querySelectorAll(".project-image-first")
-const Bg = document.querySelectorAll(".projectbg")
-const imagesList = document.querySelector(".data-images").dataset.images.split(",")
-
-const images = [
-"https://picsum.photos/900/600/?image=0", "https://picsum.photos/900/600/?image=98", "https://picsum.photos/900/600/?image=30"
-]
+const projectImageContainers = document.querySelectorAll(".projectbg")
+// const imagesList = document.querySelector(".data-images").dataset.images.split(",")
 
 var i = 0
 
-function placeImage(x,y) {
-  // const nextImage = imagesList[i].replace(/ *"*\[*\]*/g,'')
-  const nextImage = images[i]
-  const img = document.createElement("img")
-  img.setAttribute('src', nextImage)
-  img.style.position = 'absolute'
-  img.style.left = x + 'px'
-  img.style.top = y + 'px'
-  img.style.transform = 'translate(-50%, -50%) scale(0.5)'
+function placeImage(x,y, imagesList) {
+  console.log(imagesList[i], i)
+  if (imagesList[i]) {
+    // console.log('qq')
+    const nextImage = imagesList[i].replace(/ *"*\[*\]*/g,'')
+    const img = document.createElement("img")
+    img.setAttribute('src', nextImage)
+    img.style.position = 'absolute'
+    img.style.left = x + 'px'
+    img.style.top = y + 'px'
+    img.style.transform = 'translate(-50%, -50%) scale(0.5)'
 
-  document.body.appendChild(img)
+    document.body.appendChild(img)
 
-  // Bg.forEach(function(bg) {
-  //   bg.appendChild(img)
-  // })
+    // Bg.forEach(function(bg) {
+    //   bg.appendChild(img)
+    // })
 
-  i++
-  if (i >= images.length) {
-    i = 0
   }
 
+  i++
+  if (i >= imagesList.length) {
+    i = 0
+  }
 }
 
-imagesFist.forEach(function(imageFist) {
-  imageFist.addEventListener('click', function(event) {
-    event.preventDefault()
-    placeImage(event.pageX, event.pageY)
+projectImageContainers.forEach(function(projectImageContainer) {
+  const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
+  projectImageContainer.addEventListener('click', function(event) {
+    placeImage(event.pageX, event.pageY, imagesList)
   })
 })
 
+// imagesFist.forEach(function(imageFist) {
+  // document.addEventListener('click', function(event) {
+  //   event.preventDefault()
+  //   placeImage(event.pageX, event.pageY)
+  // })
+// })
 
-document.addEventListener('touchend', function(event) {
-  event.preventDefault()
-  placeImage(event.pageX, event.pageY)
-})
+
+// document.addEventListener('touchend', function(event) {
+//   event.preventDefault()
+//   placeImage(event.pageX, event.pageY)
+// })
 
 
 
