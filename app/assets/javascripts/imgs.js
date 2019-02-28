e5dbd0eb59e977b03f38f32f2e7f316e492c3e9d
My Kim Bui <mykimbuibui@gmail.com>
2019-02-28 11:42:19 +0100

    paste image in order

--- app/assets/javascripts/imgs.js
+++ app/assets/javascripts/imgs.js
@@ -7,9 +7,8 @@ const projectImageContainers = document.querySelectorAll(".projectbg")
 var i = 0
 
 function placeImage(x,y, imagesList) {
-  console.log(imagesList[i], i)
+  // console.log(imagesList[i], i)
   if (imagesList[i]) {
-    // console.log('qq')
     const nextImage = imagesList[i].replace(/ *"*\[*\]*/g,'')
     const img = document.createElement("img")
     img.setAttribute('src', nextImage)
@@ -17,13 +16,7 @@ function placeImage(x,y, imagesList) {
     img.style.left = x + 'px'
     img.style.top = y + 'px'
     img.style.transform = 'translate(-50%, -50%) scale(0.5)'
-
     document.body.appendChild(img)
-
-    // Bg.forEach(function(bg) {
-    //   bg.appendChild(img)
-    // })
-
   }
 
   i++
@@ -35,22 +28,19 @@ function placeImage(x,y, imagesList) {
 projectImageContainers.forEach(function(projectImageContainer) {
   const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
   projectImageContainer.addEventListener('click', function(event) {
+    event.preventDefault()
     placeImage(event.pageX, event.pageY, imagesList)
   })
 })
 
-// imagesFist.forEach(function(imageFist) {
-  // document.addEventListener('click', function(event) {
-  //   event.preventDefault()
-  //   placeImage(event.pageX, event.pageY)
-  // })
-// })
+projectImageContainers.forEach(function(projectImageContainer) {
+  const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
+  projectImageContainer.addEventListener('touchend', function(event) {
+    event.preventDefault()
+    placeImage(event.pageX, event.pageY, imagesList)
+  })
+})
 
-
-// document.addEventListener('touchend', function(event) {
-//   event.preventDefault()
-//   placeImage(event.pageX, event.pageY)
-// })
 });
 
 
