7108aec22b33ced7a4879192c37b7cc781edddc0
My Kim Bui <mykimbuibui@gmail.com>
2019-03-15 12:17:34 +0100

    add in img

--- app/assets/javascripts/imgs.js
+++ app/assets/javascripts/imgs.js
@@ -1,91 +1,83 @@
 document.addEventListener("DOMContentLoaded", function(){
-
   const imagesFirst = document.querySelectorAll(".project-image-first")
   const projectImageContainers = document.querySelectorAll(".projectbg")
-// const imagesList = document.querySelector(".data-images").dataset.images.split(",")
+  // const imagesList = document.querySelector(".data-images").dataset.images.split(",")
+  var i = 0
+  const scale = 0.5
 
-var i = 0
-const scale = 0.5
+  function placeImage(x,y, imagesList) {
+    // console.log(imagesList[i], i)
+    if (imagesList[i]) {
+      const nextImage = imagesList[i].replace(/ *"*\[*\]*/g,'')
+      const img = document.createElement("img")
+      img.setAttribute('src', nextImage)
 
-function placeImage(x,y, imagesList) {
-  // console.log(imagesList[i], i)
-  if (imagesList[i]) {
-    const nextImage = imagesList[i].replace(/ *"*\[*\]*/g,'')
-    const img = document.createElement("img")
-    img.setAttribute('src', nextImage)
+      const imgLoad = new Image()
 
-    const imgLoad = new Image()
+      imgLoad.addEventListener('load', function() {
+        const imgWidth = imgLoad.width * scale
+        const imgHeight = imgLoad.height * scale
 
-    imgLoad.addEventListener('load', function() {
-      const imgWidth = imgLoad.width * scale
-      const imgHeight = imgLoad.height * scale
+        projectImageContainers.forEach(function(projectImageContainer) {
+          const xBleedRight =   (x - imgWidth / 2 )  + imgWidth   - projectImageContainer.offsetWidth
+          const xBleedLeft =    (x - imgWidth / 2 )
+          const yBleedBottom =  (y - imgHeight / 2 ) + imgHeight  - projectImageContainer.offsetHeight
+          const yBleedTop =     (y - imgHeight / 2 )
+          console.log(xBleedRight, xBleedLeft)
 
-      projectImageContainers.forEach(function(projectImageContainer) {
-        const xBleedRight =   (x - imgWidth / 2 )  + imgWidth   - projectImageContainer.offsetWidth
-        const xBleedLeft =    (x - imgWidth / 2 )
-        const yBleedBottom =  (y - imgHeight / 2 ) + imgHeight  - projectImageContainer.offsetHeight
-        const yBleedTop =     (y - imgHeight / 2 )
+          img.style.position = 'absolute'
 
-        img.style.position = 'absolute'
+          if (xBleedRight > 0) {
+            img.style.left = projectImageContainer.offsetWidth - (imgWidth / 2) + 'px'
+          } else if (xBleedLeft < 0) {
+            img.style.left = (imgWidth / 2) + 'px'
+          } else {
+            img.style.left = x + 'px'
+          }
 
-        if (xBleedRight > 0) {
-          img.style.left = projectImageContainer.offsetWidth - (imgWidth / 2) + 'px'
-        } else if (xBleedLeft < 0) {
-          img.style.left = (imgWidth / 2) + 'px'
-        } else {
-          img.style.left = x + 'px'
-        }
+          if (yBleedBottom > 0) {
+            img.style.top = projectImageContainer.offsetHeight - (imgHeight / 2) + 'px'
+          } else if (yBleedTop < 0) {
+            img.style.top = (imgHeight / 2) + 'px'
+          } else {
+            img.style.top = y + 'px'
+          }
+          img.style.transform = 'translate(-50%, -50%) scale('+scale+')'
+          img.style.pointerEvents = 'none'
 
-        if (yBleedBottom > 0) {
-          img.style.top = projectImageContainer.offsetHeight - (imgHeight / 2) + 'px'
-        } else if (yBleedTop < 0) {
-          img.style.top = (imgHeight / 2) + 'px'
-        } else {
-          img.style.top = y + 'px'
-        }
-        img.style.transform = 'translate(-50%, -50%) scale('+scale+')'
-        img.style.pointerEvents = 'none'
-
-        projectImageContainer.appendChild(img)
+          projectImageContainer.appendChild(img)
+        })
       })
+
+      imgLoad.src = nextImage
+    }
+
+    i++
+    if (i >= imagesList.length) {
+      i = 0
+    }
+  }
+
+
+  projectImageContainers.forEach(function(projectImageContainer) {
+    const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
+    projectImageContainer.addEventListener('click', function(event) {
+      event.preventDefault()
+      const posLeft = event.pageX - projectImageContainer.offsetLeft
+      const posTop = event.pageY - projectImageContainer.offsetTop
+      placeImage(posLeft, posTop, imagesList)
     })
-
-    imgLoad.src = nextImage
-
-    // img.style.position = 'absolute'
-    // img.style.left = x + 'px'
-    // img.style.top = y + 'px'
-    // img.style.transform = 'translate(-50%, -50%) scale(1)'
-    // img.style.pointerEvents = 'none'
-    // projectImageContainers.appendChild(img)
-  }
-
-  i++
-  if (i >= imagesList.length) {
-    i = 0
-  }
-}
-
-
-projectImageContainers.forEach(function(projectImageContainer) {
-  const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
-  projectImageContainer.addEventListener('click', function(event) {
-    event.preventDefault()
-    const posLeft = event.pageX - projectImageContainer.offsetLeft
-    const posTop = event.pageY - projectImageContainer.offsetTop
-    placeImage(posLeft, posTop, imagesList)
   })
-})
 
-projectImageContainers.forEach(function(projectImageContainer) {
-  const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
-  projectImageContainer.addEventListener('touchend', function(event) {
-    event.preventDefault()
-    const posLeft = event.pageX - projectImageContainer.offsetLeft
-    const posTop = event.pageY - projectImageContainer.offsetTop
-    placeImage(posLeft, posTop, imagesList)
+  projectImageContainers.forEach(function(projectImageContainer) {
+    const imagesList = projectImageContainer.querySelector(".data-images").dataset.images.split(",")
+    projectImageContainer.addEventListener('touchend', function(event) {
+      event.preventDefault()
+      const posLeft = event.pageX - projectImageContainer.offsetLeft
+      const posTop = event.pageY - projectImageContainer.offsetTop
+      placeImage(posLeft, posTop, imagesList)
+    })
   })
-})
 
 })
 
