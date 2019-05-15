ff4ed314056e57d507d38d23f04d67a7aaa53639
My Kim Bui <mykimbuibui@gmail.com>
2019-05-15 13:09:33 +0200

    add transitions

--- app/assets/javascripts/bubble.js
+++ app/assets/javascripts/bubble.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const bubbleit = document.querySelector('.wreckit')
+  const arrowTop = document.querySelector('arrow-top')
 
   bubbleit.addEventListener('mouseover', function() {
     bubbleit.classList.add('shake-bottom')
+    arrowTop.classList.add('bounce-top')
   })
 
   bubbleit.addEventListener('mouseout', function() {
     bubbleit.classList.remove('shake-bottom')
+    arrowTop.classList.remove('bounce-top')
   })
 })
