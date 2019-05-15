299b2dc9e05d4a8db92c26097faee012524ac758
My Kim Bui <mykimbuibui@gmail.com>
2019-05-15 13:00:40 +0200

    change clickme

--- app/assets/javascripts/clickme.js
+++ app/assets/javascripts/clickme.js
@@ -7,46 +7,45 @@ document.addEventListener("DOMContentLoaded", function() {
     item.addEventListener("mouseover", function () {
       // event.preventDefault()
 
-      const posx = Math.random() * item.offsetWidth;
-      const posy = Math.random() * item.offsetHeight;
+      const posx = Math.random() * item.offsetWidth
+      const posy = Math.random() * item.offsetHeight
 
-      // const posx = event.pageX / item.offsetWidth;
-      // const posy = event.pageY / item.offsetHeight;
-      // const posx = event.pageX - (item.offsetWidth / 2);
-      // const posy = event.pageY - (item.offsetHeight / 2);
+      // const posx = event.pageX / item.offsetWidth
+      // const posy = event.pageY / item.offsetHeight
+      // const posx = event.pageX - (item.offsetWidth / 2)
+      // const posy = event.pageY - (item.offsetHeight / 2)
 
-      const newClick = document.createElement('div');
-      newClick.className = 'clickme';
-      newClick.innerHTML = 'click click!';
-      newClick.style.color = 'white';
-      newClick.style.fontSize = '30px';
-      newClick.style.fontFamily = 'SpaceGrotesk-Regular';
-      newClick.style.position = 'absolute';
-      newClick.style.left = posx.toFixed(1) + 'px';
-      newClick.style.top = posy.toFixed(1) + 'px';
-      newClick.style.WebkitFilter = "contrast(40)";
-      newClick.style.filter = "contrast(40)";
+      const newClick = document.createElement('div')
+      newClick.classList.add('clickme')
+      newClick.innerHTML = 'click click!'
+      newClick.style.position = 'absolute'
+      newClick.style.color = '#2f2f2f'
+      newClick.style.mixBlendMode = 'color-burn'
+      newClick.style.fontFamily = 'BusinessHistory-Regular'
+      newClick.style.fontSize = '30px'
+      newClick.style.left = posx.toFixed(1) + 'px'
+      newClick.style.top = posy.toFixed(1) + 'px'
 
       function fadeOutEffect() {
 
         var fadeEffect = setInterval(function () {
           if (!newClick.style.opacity) {
-            newClick.style.opacity = 1;
+            newClick.style.opacity = 1
           }
           if (newClick.style.opacity > 0) {
-            newClick.style.opacity -= 0.1;
+            newClick.style.opacity -= 0.1
           } else {
-            clearInterval(fadeEffect);
+            clearInterval(fadeEffect)
           }
-        }, 100);
+        }, 100)
       }
 
-      item.appendChild(newClick);
-      fadeOutEffect(newClick);
+      item.appendChild(newClick)
+      fadeOutEffect(newClick)
 
-      const allClicks = document.querySelectorAll(".clickme");
+      const allClicks = document.querySelectorAll(".clickme")
 
-      Math.min(Math.max(parseInt(allClicks.length), 10), 30);
+      Math.min(Math.max(parseInt(allClicks.length), 10), 30)
 
       // if (allClicks.length < 5) {
       //   item.appendChild(newClick);
